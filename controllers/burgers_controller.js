cf173e05f5f1da43b6d70c3a8ca9723762f7fce5
dsuare1 <suarez.derrick@gmail.com>
2016-08-05 12:08:50 -0500

    tier 1 functionality 100%; removed yelp layout structure

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,21 +29,31 @@ router.put('/burgers/update/:id', function(req, res) {
     });
 });
 
-// router.put('/burgers/replace/:id', function(req, res) {
-//     var queryCondition = 'id = ' + req.params.id;
+router.put('/burgers/replace/:id', function(req, res) {
+    var burgerID = req.params.id;
 
-//     burger.replaceOne(queryCondition, function() {
-//         res.redirect('/');
-//     });
-// });
+    burgers.update({
+        devoured: req.body.devoured
+    }, {
+        where: {
+            id: burgerID
+        }
+    }).then(function() {
+        res.redirect('/');
+    });
+});
 
-// router.delete('/burgers/delete/:id', function(req, res) {
-//     var queryCondition = 'id = ' + req.params.id;
+router.delete('/burgers/delete/:id', function(req, res) {
+    var burgerID = req.params.id;
 
-//     burger.deleteOne(queryCondition, function() {
-//         res.redirect('/');
-//     })
-// })
+    burgers.destroy({
+        where: {
+            id: burgerID
+        }
+    }).then(function() {
+        res.redirect('/');
+    });
+})
 
 router.post('/burgers/create', function(req, res) {
     burgers.create({ burger_name: req.body.burger_name, devoured: req.body.devoured }).then(function() {
